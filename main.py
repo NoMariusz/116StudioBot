06db946eb73f692a0e5ea3862fa6466baab98dab
NoMariusz <mariusz2727@vp.pl>
2020-07-21 21:27:37 +0200

    Added meeting event to bot

--- main.py
+++ main.py
@@ -1,34 +1,87 @@
 import discord
+import asyncio
 from src.commands import *
-from src.constants import *
+from src.json_helper_modul import JsonHelper
 
 
-class Discord116Bot:
-    def __init__(self):
-        self.client = discord.Client()
+class Discord116Bot(discord.Client):
+    def __init__(self, **options):
+        super().__init__(**options)
         self.token = self.get_token()
+        self.meeting_event = None
 
-        @self.client.event
-        async def on_message(message):
-            print("BOT: get message '%s' by '%s'" % (message.content, message.author))
+    async def on_message(self, message):
+        print("BOT: get message '%s' by '%s'" % (message.content, message.author))
 
-            for command in commands_dict.keys():
-                if message.content.find(command) == 0:
-                    if not commands_dict[command]["parametrized"]:
-                        if command == str(message.content):
-                            print("BOT: get command %s" % message.content)
-                            await commands_dict[message.content]["command"](self.client, message)
-                    else:
+        for command in commands_dict.keys():
+            if message.content.find(command) == 0:
+                if not commands_dict[command]["parametrized"]:
+                    if command == str(message.content):
                         print("BOT: get command %s" % message.content)
-                        await commands_dict[command]["command"](self.client, message)
+                        await commands_dict[message.content]["command"](self, message)
+                else:
+                    print("BOT: get command %s" % message.content)
+                    await commands_dict[command]["command"](self, message, command=command)
 
-    def run(self):
-        self.client.run(self.token)
+    async def on_ready(self):
+        print("BOT: ready")
+        await self.init_meeting_event()
+
+    def run(self, *args, **kwargs):
+        super().run(self.token)
 
     @staticmethod
     def get_token():
         with open("data/token.txt", "r") as file:
             return file.read()
+
+    async def init_meeting_event(self):
+        if JsonHelper.get_bot_data_dict()["next_meeting"] is not None:
+            self.meeting_event = asyncio.create_task(self.made_meeting_event())
+            await self.meeting_event
+        else:
+            print("BOT: init_meeting_event() - meeting date null")
+
+    async def made_meeting_event(self):
+        meeting_date_str = JsonHelper.get_bot_data_dict()["next_meeting"]
+        meeting_date = datetime.datetime.strptime(meeting_date_str, DATETIME_FORMAT)
+        now_date = datetime.datetime.now().utcnow()
+        time_to_meeting = meeting_date - now_date
+        print("BOT: made_meeting_event() - time to meeting %s" % time_to_meeting)
+        await discord.utils.sleep_until(meeting_date)
+        print("BOT: made_meeting_event() - meeting going to start")
+        await self.on_meeting_start()
+
+    async def on_meeting_start(self):
+        print("BOT: on_meeting_start()")
+        send_message = None
+        for channel in self.get_all_channels():
+            if channel.name == "bot":
+                print("BOT: meeting start")
+                send_message = await channel.send("@everyone 116 Studio meeting stared !!! :kissing_heart:")
+                await channel.send(":heart: :heart: :heart: Good Luck :heart: :heart: :heart:")
+                break
+        else:
+            print("BOT: ERROR meeting start - not find bot channel")
+
+        bot_data_dict = JsonHelper.get_bot_data_dict()
+        bot_data_dict["last_meeting"] = bot_data_dict["next_meeting"]
+        bot_data_dict["next_meeting"] = None
+        if send_message is None:
+            time = await display_michal_lol_time(self, message=send_message, quiet=True)
+        else:
+            time = await display_michal_lol_time(self, message=send_message)
+        bot_data_dict["last_meeting_michal_lol_hours_time"] = time
+        print("BOT: meeting_event_start - new bot_data_dict: %s" % bot_data_dict)
+        JsonHelper.save_bot_data_dict(bot_data_dict)
+
+        self.meeting_event = None
+
+    async def reload_meeting_event(self):
+        if self.meeting_event is not None:
+            self.meeting_event.cancel()
+        print("BOT: reload_meeting_event() - cancel meeting event")
+        await self.init_meeting_event()
 
 
 Discord116Bot().run()
--- src/commands.py
+++ src/commands.py
@@ -5,63 +5,84 @@ from src.web_info_modul import WebInfoModul
 from src.json_helper_modul import JsonHelper
 
 
-async def make_help(client: discord.Client, message: discord.Message):
-    help_embed = discord.Embed(title="Help for 116 Studio Bot",
+async def make_help(client, message: discord.Message, **kwargs):
+    help_embed = discord.Embed(title="Help for 116Studio Bot",
                                description="Help page have list of all commands and descriptions to it")
     for command, info in commands_dict.items():
         help_embed.add_field(name=command, value=info["description"])
     await message.channel.send(embed=help_embed)
 
 
-async def on_stupki(client: discord.Client, message: discord.Message):
-    await message.channel.send("Michał")
+async def on_stupki(client, message: discord.Message, **kwargs):
+    await message.channel.send("Michał :rofl:")
 
 
-async def display_michal_lol_time(client: discord.Client, message: discord.Message):
+async def display_michal_lol_time(client, message: discord.Message, **kwargs):
     bot_data = JsonHelper.get_bot_data_dict()
     time = WebInfoModul.get_user_lol_play_time("critovork")
+
+    if "quiet" in kwargs.keys():
+        if kwargs["quiet"]:
+            return time
 
     hour_played_since_last_meeting = time - bot_data["last_meeting_michal_lol_hours_time"]
     time_since_last_meeting = datetime.datetime.now() - datetime.datetime.strptime(bot_data["last_meeting"], DATETIME_FORMAT)
     days_since_last_meeting = time_since_last_meeting.days if time_since_last_meeting.days != 0 else 1
     lol_hours_per_day = hour_played_since_last_meeting / days_since_last_meeting
-    await message.channel.send("Michał (critovork) has play in lol %s hours overall\n and has play in lol %s hours from last meeting at %s\n this is %s hours per day"
+    await message.channel.send("Michał (critovork) :hot_face: :foot: has play in lol %s hours overall :thinking:\n and has play in lol %s hours from last meeting at %s\n this is %s hours per day :scream:"
                                % (time, hour_played_since_last_meeting, bot_data["last_meeting"][:-9], lol_hours_per_day))
+    return time
 
 
-async def display_player_lol_time(client: discord.Client, message: discord.Message):
-    nick = str(message.content)[12:]
+async def display_player_lol_time(client, message: discord.Message, **kwargs):
+    command_len = 0
+    if "command" in kwargs.keys():
+        command_len = len(kwargs["command"]) + 1
+    else:
+        print("BOT: ERROR - parametrized command function not get 'command' arg")
+
+    nick = str(message.content)[command_len:]
     time = WebInfoModul.get_user_lol_play_time(nick)
     if time is None:
-        await message.channel.send("I can't find player %s" % nick)
+        await message.channel.send("I can't find player %s :cry:" % nick)
     else:
-        await message.channel.send("%s played on lol %s hours overall" % (nick, time))
+        await message.channel.send(":video_game: %s played on lol %s hours overall :face_with_monocle: " % (nick, time))
 
 
-async def set_next_meeting_date(client: discord.Client, message: discord.Message):
-    date_srt = str(message.content)[18:]
+async def set_next_meeting_date(client, message: discord.Message, **kwargs):
+    command_len = 0
+    if "command" in kwargs.keys():
+        command_len = len(kwargs["command"]) + 1
+    else:
+        print("BOT: ERROR - parametrized command function not get 'command' arg")
+
+    date_srt = str(message.content)[command_len:]
     try:
         datetime.datetime.strptime(date_srt, DATETIME_FORMAT)
     except Exception as ex:
         print("Commands: set_next_meeting_date() - Error\n %s" % ex)
-        await message.channel.send("%s is in bad datetime format to set meeting datetime\n Correct format is %s" % (date_srt, DATETIME_FORMAT))
+        await message.channel.send("%s is in bad datetime format to set meeting datetime :dizzy_face:\n Correct format is %s" % (date_srt, DATETIME_FORMAT))
     else:
         data_dict = JsonHelper.get_bot_data_dict()
         data_dict["next_meeting"] = date_srt
         JsonHelper.save_bot_data_dict(data_dict)
-        await message.channel.send("New meeting datetime %s was set" % date_srt)
+        await message.channel.send("New meeting datetime %s was set :smirk:" % date_srt)
+        await client.reload_meeting_event()
 
 
-async def display_next_meeting_date(client: discord.Client, message: discord.Message):
+async def display_next_meeting_date(client, message: discord.Message, **kwargs):
     data_dict = JsonHelper.get_bot_data_dict()
-    await message.channel.send("Next meeting is at %s" % data_dict["next_meeting"])
+    if data_dict["next_meeting"] is None:
+        await message.channel.send("Next meeting date isn't set yet :frowning:")
+    else:
+        await message.channel.send("Next meeting is at %s in UTC :partying_face:" % data_dict["next_meeting"])
 
 
 commands_dict = {
-    COMMAND_BOT_SIGN + "stupka": {"description": "Say wjo like foots", "command": on_stupki, "parametrized": False},
+    COMMAND_BOT_SIGN + "stupka": {"description": "Display who like foots", "command": on_stupki, "parametrized": False},
     COMMAND_BOT_SIGN + "help": {"description": "Display help about bot", "command": make_help, "parametrized": False},
     COMMAND_BOT_SIGN + "michal_lol": {"description": "Display info about how much michal play lol", "command": display_michal_lol_time, "parametrized": False},
     COMMAND_BOT_SIGN + "played_lol": {"description": "Display info about how much given player play on lol", "command": display_player_lol_time, "parametrized": True},
-    COMMAND_BOT_SIGN + "set_next_meeting": {"description": "Set date of next meeting, please use date in format %s" % DATETIME_FORMAT, "command": set_next_meeting_date, "parametrized": True},
-    COMMAND_BOT_SIGN + "next_meeting": {"description": "Display when is next meeting", "command": display_next_meeting_date, "parametrized": False}
+    COMMAND_BOT_SIGN + "set_next_meeting": {"description": "Set date of next meeting, please use date in format %s\nRemember: set date in UTC time zone" % DATETIME_FORMAT, "command": set_next_meeting_date, "parametrized": True},
+    COMMAND_BOT_SIGN + "next_meeting": {"description": "Display when is next meeting\nRemember: date should be in UTC time zone", "command": display_next_meeting_date, "parametrized": False}
 }
